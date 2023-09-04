ce04e8bba1e29b2f003c67d3bf8d3cee6117ce60
sindpet <137641233+sindpet@users.noreply.github.com>
2023-09-04 10:28:40 +0200

    Small layout improvements

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
   def profile_image(user, options={})
-    image_tag "https://doodleipsum.com/700/avatar-2?i=bf8b97258fa53643f64755c68b08041b", class: options[:class]
+    image_tag "https://doodleipsum.com/700/avatar-2?i=bf8b97258fa53643f64755c68b08041b", class: options[:class] + " rounded-circle"
   end
 end
