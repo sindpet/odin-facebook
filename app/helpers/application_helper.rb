047a4eda053e8075be97f6a3894bbcf1834da24f
sindpet <137641233+sindpet@users.noreply.github.com>
2023-09-05 23:40:31 +0200

    Visual updates and fixes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,16 @@
 module ApplicationHelper
-  def profile_image(user, options={})
-    classes = options[:class] + " rounded-circle"
+  def profile_image(user)
+    user.profile_pic_url? ? user.profile_pic_url : "https://doodleipsum.com/700/avatar-2?i=bf8b97258fa53643f64755c68b08041b"
 
-    if user.profile_pic_url?
-      image_tag user.profile_pic_url, class: classes
-    else
-      image_tag "https://doodleipsum.com/700/avatar-2?i=bf8b97258fa53643f64755c68b08041b", class: classes
-    end
+    ##options[:class] = "" if options[:class].nil?
+#
+    #classes = "rounded-circle " + options[:class]
+#
+    #if user.profile_pic_url?
+    #  image_tag user.profile_pic_url, class: classes
+    #else
+    #  image_tag "https://doodleipsum.com/700/avatar-2?i=bf8b97258fa53643f64755c68b08041b", class: classes
+    #end
 
   end
 end
